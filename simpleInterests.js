11610cae1932ac6696a4ee90318a50496980d3a2
swaleha2002 <120295486+swaleha2002@users.noreply.github.com>
2023-10-15 14:50:18 +0530

    Create simpleInterests.js

---
+++ simpleInterests.js
@@ -0,0 +1,17 @@
+//program to calculate simple interest based on principal, rate, and time.
+function calculateSimpleInterest(principal, rate, time) {
+  // Simple Interest formula: SI = (P * R * T) / 100
+  const simpleInterest = (principal * rate * time) / 100;
+  return simpleInterest;
+}
+
+// Example values
+const principalAmount = 1000; // Principal amount in dollars
+const annualInterestRate = 5; // Annual interest rate in percent
+const timeInYears = 2; // Time in years
+
+// Calculate simple interest
+const interest = calculateSimpleInterest(principalAmount, annualInterestRate, timeInYears);
+
+// Print the result
+console.log('Simple Interest:', interest);
